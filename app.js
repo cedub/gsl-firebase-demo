273c494631a1f475d6d56f422f7662f1411a8f44
Christian West <west.christianj@gmail.com>
2017-06-30 15:43:18 -0300

    Tweaks to add firebase support to the code

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-/* Set up your hello world app here, but then serve up index.html instead */
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use('/static', express.static('static'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
